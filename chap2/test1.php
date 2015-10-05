cd083eb3e8313c67515eaf5436e0141c909a6b60
xxxknight <xingshiyang24@163.com>
2015-10-06 00:12:46 +0800

    commit

--- chap2/test1.php
+++ chap2/test1.php
@@ -5,8 +5,8 @@
  * To change the template for this generated file go to
  * Window - Preferences - PHPeclipse - PHP - Code Templates
  */
- $DOCUMENT_ROOT = $_SERVER['DOCUMENT_ROOT'];
- echo $DOCUMENT_ROOT;
- 
+$DOCUMENT_ROOT = $_SERVER['DOCUMENT_ROOT'];
+echo $DOCUMENT_ROOT;
+
 ?>
 <?php echo gethostbyname($_ENV['COMPUTERNAME']);?>
